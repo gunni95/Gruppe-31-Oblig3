db7f3ce79bcf1dcb8f58859a3d96badd8cd48f71
669802 <669802@stud.hvl.no>
2023-03-21 14:12:51 +0100

    Forsøk

--- src/no/hvl/dat102/Oppg1/Oppgave1.java
+++ src/no/hvl/dat102/Oppg1/Oppgave1.java
@@ -1,13 +1,32 @@
 package no.hvl.dat102.Oppg1;
 
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Oppgave1 {
     public static void main(String[] args){
+        Random random = new Random();
 
         boolean funnet = false;
-        int j = 0;
+        int j = 99999;
+        int[] ints = random.ints(100000,0, 1000000).toArray();
 
-        while (!funnet && j>=0){
+        int[] oppgA = Arrays.copyOf(ints, j);
 
+        while (!funnet && j > 1){
+            int a = oppgA[j-1];
+            int b = oppgA[j-2];
+
+            if (a < b) {
+                oppgA[j-1] = b;
+                oppgA[j-2] = a;
+            }
+            j--;
+        }
+
+        for (int i : oppgA) {
+            System.out.println(i);
         }
     }
 
