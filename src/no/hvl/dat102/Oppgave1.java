b5e8e6e41434dbd14f916713891d7c021edbfa16
669802 <669802@stud.hvl.no>
2023-03-21 12:55:08 +0100

    Lagt til utgangspunkt for oppgave 1

--- src/no/hvl/dat102/Oppgave1.java
+++ src/no/hvl/dat102/Oppgave1.java
@@ -1,5 +1,14 @@
 package no.hvl.dat102;
 
 public class Oppgave1 {
-    boolean funnet = false;
+    public static void main(String[] args){
+
+        boolean funnet = false;
+        int j = 0;
+
+        while (!funnet && j>=0){
+
+        }
+    }
+
 }
